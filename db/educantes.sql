fb8e8995bf9d8352cc68c84d785788d229d1db86
David Pérez <david.pe.ra.1996@gmail.com>
2018-03-09 18:16:55 +0100

    base de datos en proceso

--- db/educantes.sql
+++ db/educantes.sql
@@ -2,3 +2,24 @@
 -- Archivo de base de datos --
 ------------------------------
 
+DROP TABLE IF EXISTS alumnos CASCADE;
+
+CREATE TABLE alumnos
+(
+    id      bigserial       PRIMARY KEY
+  , nombre  varchar(255)    NOT NULL
+);
+
+DROP TABLE IF EXISTS usuarios CASCADE;
+
+CREATE TABLE usuarios
+(
+    id          bigserial       PRIMARY KEY
+  , nombre      varchar(255)    NOT NULL UNIQUE
+  , password    varchar(255)    NOT NULL
+  , email       varchar(255)
+  , tel_movil   numeric(9)      NOT NULL UNIQUE
+  , rol         char(1)         NOT NULL
+  , alumno_id   bigint          REFERENCES alumnos (id)
+                                ON DELETE NO ACTION ON UPDATE CASCADE
+);
