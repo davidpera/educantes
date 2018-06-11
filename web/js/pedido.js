3332c6ac93ab94f9dc5a2c613c30d3180b63e6c5
David Pérez Ramírez de Arellano <david.pe.ra.1996@gmail.com>
2018-06-11 14:34:38 +0200

    documentacion y arreglos ventana

--- web/js/pedido.js
+++ web/js/pedido.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
 function eventoPedidoMultiple(){
     $('#pedidoMult').on('click', function(){
         var le = ($(window).width()/2)-150;
-        var to = ($(window).height()/2)-50;
+        var to = ($(window).height()/2)-250;
         var options = "width=400px, height=500px,top="+to+",left="+le+", menubar=no, resizable=no,scrollbars=yes";
         var nueva = window.open('/js/ventana.html','Pedido',options);
     });
