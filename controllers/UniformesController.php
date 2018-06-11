9e09fc0859c171066dd8e93750b4f8c36bb5c638
David Pérez Ramírez de Arellano <david.pe.ra.1996@gmail.com>
2018-06-11 13:45:17 +0200

    arreglos

--- controllers/UniformesController.php
+++ controllers/UniformesController.php
@@ -35,7 +35,7 @@ class UniformesController extends Controller
             ],
             'access' => [
                 'class' => \yii\filters\AccessControl::className(),
-                'only' => ['create', 'update', 'view'],
+                'only' => ['create', 'update', 'view', 'index'],
                 'rules' => [
                     [
                         'allow' => true,
